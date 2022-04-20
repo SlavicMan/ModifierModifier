e330a8de3451799a7435b1c59f473f118a4b3de2
JФSԐPЊҤЇ ҠЯДԞФЩSҜЇ <kizz4114@yahoo.com>
2022-04-20 19:02:37 +0930

    Adding everything

---
+++ ModifierModifier/Main.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+using CobwebAPI;
+using CobwebAPI.API;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+
+namespace ModifierModifier
+{
+    [BepInPlugin(ModName, ModGUID, ModVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        public static readonly GUIStyle backgroundStyle = new GUIStyle();
+        public static readonly GUIStyle GUIStyle = new GUIStyle();
+        
+        private Vector2 currentDrag = new Vector2();
+        public Vector2 scrollPosition;
+        public bool menuEnabled;
+        
+        public const string ModName = "ModifierModifier";
+        public const string ModAuthor = "Bazinga";
+        public const string ModGUID = "com.bazinga.modifiermodifier";
+        public const string ModVersion = "1.0.0";
+        internal Harmony Harmony;
+
+        internal void Awake()
+        {
+            Harmony = new Harmony(ModGUID);
+
+            Harmony.PatchAll();
+            Logger.LogInfo($"{ModName} successfully loaded! Made by {ModAuthor}");
+
+
+            Main.GUIStyle.alignment = TextAnchor.MiddleCenter;
+            Main.GUIStyle.fontSize = 16;
+            Main.GUIStyle.fontStyle = FontStyle.Bold;
+            Main.GUIStyle.normal.background = Texture2D.grayTexture;
+            backgroundStyle.alignment = TextAnchor.MiddleCenter;
+            backgroundStyle.normal.textColor = Color.black;
+            backgroundStyle.normal.background = Texture2D.grayTexture;
+            menuEnabled = false;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Insert))
+            {
+                menuEnabled = !menuEnabled;
+            }
+        }
+        
+        [HarmonyPatch(typeof(ModifierManager), "GetNonMaxedWavesMods")]
+        private void OnGUI()
+        {
+
+            var menuRect = new Rect(0, 25f, Screen.width / 2, Screen.height / 2);
+            GUI.BeginGroup(menuRect, GUIStyle);
+            int valX = 0;
+            int valY = 0;
+            foreach (var mods in GetTotalNonMaxedModifiers())
+            {
+                if ((valY + 77) >= (Screen.height / 2))
+                {
+                    valY = 0;
+                    valX += 173;
+                }
+
+                bool flag = GUI.Button(new Rect(25f + valX, (float) (25 + valY), 150f, 40f), mods.data.name,
+                    Main.backgroundStyle);
+                if (flag)
+                {
+                    Logger.LogInfo(mods.data.name + "Has been clicked");
+                    mods.levelInWaves = mods.data.maxLevel;
+
+                    Logger.LogInfo(mods.levelInWaves);
+                    Logger.LogInfo(mods.levelInVersus);
+                }
+
+                valY += 47;
+            }
+            GUI.EndGroup();
+
+
+        }
+
+        void ModifierModifierWindow(int windowID)
+        {
+
+
+        }
+
+        public void MaxMod(Modifier mod)
+        {
+            WaveModifiers.Give(mod.data.name, mod.data.maxLevel);
+            Logger.LogInfo(mod.levelInWaves);
+            Logger.LogInfo(mod.levelInVersus);
+        }
+        public List<Modifier> GetTotalNonMaxedModifiers()
+        {
+            var list = new List<Modifier>();
+            list.AddRange(ModifierManager.instance.GetVersusMods());
+            list.AddRange(ModifierManager.instance.GetNonMaxedWavesMods());
+            return list;
+        }
+
+    }
+
+    [HarmonyPatch(typeof(ModifierManager), "GetNonMaxedWavesMods")]
+    internal class ModifierManagerGetNonMaxedWavesModsPatch
+    {
+        internal static List<Modifier> Mods { get; private set; } = new();
+
+        internal static bool Prefix(ModifierManager __instance, ref List<Modifier> __result)
+        {
+            var templist = (from m in Traverse.Create(__instance).Field<List<Modifier>>("_modifiers").Value
+                where m.levelInWaves < m.data.maxLevel && m.data.waves
+                select m).ToList();
+            if (Mods.Count > 0)
+            {
+                foreach (Modifier mod in Mods)
+                {
+                    if (mod == null || templist.Contains(mod))
+                        continue;
+
+                    templist.Add(mod);
+                }
+            }
+
+            __result = templist;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(VersionNumberTextMesh), "Start")]
+        public class VersionNumberTextMeshPatch
+        {
+            public static void Postfix(VersionNumberTextMesh __instance)
+            {
+                __instance.textMesh.text +=
+                    $"\n<color=red>{Main.ModName} v{Main.ModVersion} by {Main.ModAuthor}</color>";
+            }
+        }
+    }
+    
+    // BIN
+    // ON GUI SHIT
+    // GUI.BeginGroup(new Rect(25f, 25f,Screen.width/2,Screen.height/2), Main.backgroundStyle);
+    // foreach (var mods in GetTotalNonMaxedModifiers())
+    // {
+    //
+    // }
+    // GUI.EndGroup();
+}
