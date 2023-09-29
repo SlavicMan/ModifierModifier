793e3b777226ef9b0a0d9c6c1d39022f5ac48394
viadot <kizz4224@yahoo.com>
2023-09-30 00:49:27 +0930

    - Override maximum mod level - Overhauled UI - Scrollable Modifier List - Removing Modifiers

--- ModifierModifier/Main.cs
+++ ModifierModifier/Main.cs
@@ -6,177 +6,294 @@ using HarmonyLib;
 using CobwebAPI.API;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Data;
+using System.Reflection;
+using System.Security.Cryptography;
+
 
 namespace ModifierModifier
 {
+
     [BepInPlugin(ModName, ModGUID, ModVersion)]
     public class Main : BaseUnityPlugin
     {
-        public static readonly GUIStyle backgroundStyle = new GUIStyle();
-        public static readonly GUIStyle GUIStyle = new GUIStyle();
+
+
+        private Texture2D LoadTextureFromAssembly(string textName)
+        {
+            Stream inTex = Assembly.GetExecutingAssembly().GetManifestResourceStream($"ModifierModifier.Resources.{textName}.png"); // Load Embedded PNG
+            byte[] inBytes = inTex.ReadAllBytes(); // Convert Stream to Bytes
+            Texture2D tempTex = new Texture2D(14, 14); // Empty Texture
+            ImageConversion.LoadImage(tempTex, inBytes);
+            return tempTex;
+        }
+
         
-        //private Vector2 currentDrag = new Vector2();
-        public Vector2 scrollPosition;
+        public static GUIStyle backgroundStyle = new GUIStyle();
+        public static GUIStyle GUIStyle        = new GUIStyle();
+        public static GUIStyle btnStyle        = new GUIStyle();
+
         public bool menuEnabled;
+        public bool gotModifiers;
         public bool overrideMaxLevel;
         public int addValue = 1;
         public bool devConsoleEnabled;
         private InputAction _MenuAction = new InputAction(binding: "<Keyboard>/Insert");
-        // private InputAction _ConsoleAction = new InputAction("<Keyboard>/Grave");
-        
+
         public const string ModName = "ModifierModifier";
-        public const string ModAuthor = "JustAWaffler";
-        public const string ModGUID = "JustAWaffler.modifiermodifier";
+        public const string ModAuthor = "V1AC";
+        public const string ModGUID = "V1AC.modifiermodifier";
         public const string ModVersion = "1.2.0";
-        internal Harmony Harmony;
+
+        // position;// = new Vector2(0, 38.5f);
+        //Rect ModifierRect;// = new Rect(5, 5, 362, 462);
+
+        Texture2D toggleActive = new Texture2D(14, 14);
+        Texture2D toggleHover = new Texture2D(14, 14);
+        Texture2D toggleOn = new Texture2D(14, 14);
+        Texture2D toggleOnActive = new Texture2D(14, 14);
+        Texture2D toggleOnHover = new Texture2D(14, 14);
+        Texture2D toggle = new Texture2D(14, 14);
+        Texture2D uimessy = new Texture2D(512, 512);
+        Texture2D buttonOn = new Texture2D(14, 14);
+        Texture2D buttonHover = new Texture2D(14, 14);
+        Texture2D buttonActive = new Texture2D(14, 14);
+        Texture2D buttonNormal = new Texture2D(14, 14);
+        Texture2D buttonOnHover = new Texture2D(14, 14);
+        object[] resources = Resources.LoadAll("");
+
+        private List<Modifier> modifiers;
+        internal Harmony? Harmony;
 
         internal void Awake()
         {
+            
+            if (Assembly.GetExecutingAssembly().GetManifestResourceStream("ModifierModifier.Resources.toggleactive.png") != null)
+            {
+                Logger.LogMessage("loaded Resources//toggleactive.png");
+            }
+            else
+            {
+                Logger.LogMessage("Could not load Resources//toggleactive.png");
+            }
             Harmony = new Harmony(ModGUID);
 
             Harmony.PatchAll();
-            Logger.LogMessage($"{ModName} successfully loaded! Made by {ModAuthor} \n Toggle Menu with the INS or INSERT key");
+            Logger.LogMessage($"{ModName} successfully loaded! Made by {ModAuthor}");
 
+            
+
+            toggleActive =      LoadTextureFromAssembly("toggleactive");
+            toggleOnActive =    LoadTextureFromAssembly("toggleonactive");
+            toggleHover =       LoadTextureFromAssembly("togglehover");
+            toggleOnHover =     LoadTextureFromAssembly("toggleonhover");
+            toggleOn =          LoadTextureFromAssembly("toggleon");
+            toggle =            LoadTextureFromAssembly("toggle");
+            buttonOn =          LoadTextureFromAssembly("buttonon");
+            buttonHover =       LoadTextureFromAssembly("buttonhover");
+            buttonNormal =      LoadTextureFromAssembly("button");
+            buttonActive =      LoadTextureFromAssembly("buttonactive");
+            buttonOnHover =     LoadTextureFromAssembly("buttononhover");
+            uimessy =           LoadTextureFromAssembly("uimessy");
 
             Main.GUIStyle.alignment = TextAnchor.MiddleCenter;
             Main.GUIStyle.fontSize = 16;
             Main.GUIStyle.fontStyle = FontStyle.Bold;
             Main.GUIStyle.normal.background = Texture2D.grayTexture;
+
+
             backgroundStyle.alignment = TextAnchor.MiddleCenter;
             backgroundStyle.normal.textColor = Color.black;
-            backgroundStyle.normal.background = Texture2D.grayTexture;
+            backgroundStyle.normal.background = uimessy;
+            
+
             menuEnabled = false;
             _MenuAction.Enable();
-            //_ConsoleAction.Enable();
+            btnStyle = new GUIStyle
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 16,
+                fontStyle = FontStyle.Bold,
+                fixedHeight = 146,
+                fixedWidth = 77,
+                stretchHeight = false,
+                stretchWidth = false,
+                onActive =
+                {
+                    background = buttonOn
+                },
+                onHover =
+                {
+                    background = buttonOnHover
+                },
+                onNormal = 
+                {
+                    background = buttonOn
+                },
+                hover =
+                {
+                    background = buttonHover
+                },
+                normal =
+                {
+                    background = buttonNormal
+                },
+                active =
+                {
+                    background = buttonActive
+                }
+            };
         }
 
         private void Update()
         {
             if (_MenuAction.triggered)
-            {
-                Logger.LogMessage($"Menu Toggled: {menuEnabled}");
                 menuEnabled = !menuEnabled;
-            }
 
-            // if (_ConsoleAction.triggered)
-            // {
-            //     devConsoleEnabled = !devConsoleEnabled;
-            // }
         }
-        
-        [HarmonyPatch(typeof(ModifierManager), "GetNonMaxedSurvivalMods")]
+
         private void OnGUI()
         {
-            var menuRect = new Rect(0, 25f, Screen.width / 2, Screen.height / 2);
-
-            int valX = 0;
-            int valY = 0;
+            
+            List<GameObject> btns = new List<GameObject>();
             if (menuEnabled)
             {
-                GUI.BeginGroup(menuRect, GUIStyle);
-
-                foreach (var mods in GetTotalNonMaxedModifiers())
+                if (!gotModifiers)
                 {
-                    if ((valY + 77) >= (Screen.height / 2))
-                    {
-                        valY = 0;
-                        valX += 173;
-                    }
-
-
-                    overrideMaxLevel = GUI.Toggle(new Rect(0, -10, 70, 10), overrideMaxLevel, "Override Max Level");
-                    bool flag = GUI.Button(new Rect(25f + valX, (float) (25 + valY), 150f, 40f), mods.data.name + " : " + mods.levelInSurvival,
-                        Main.backgroundStyle);
-                    if (flag)
-                    {
-                        Logger.LogMessage(mods.data.name + "Has been clicked");
-                        if (mods.levelInSurvival < mods.data.maxLevel)
-                        {
-                            mods.levelInSurvival += addValue;
-                        } else if (overrideMaxLevel)
-                        {
-                            mods.levelInSurvival += addValue;
-                        } else if (mods.levelInSurvival > mods.data.maxLevel & !overrideMaxLevel)
-                        {
-                            mods.levelInSurvival = mods.data.maxLevel;
-                        }
-                        
-                        
-                        //mods.levelInSurvival = mods.data.maxLevel;
-
-                        Logger.LogMessage(mods.levelInSurvival);
-                        Logger.LogMessage(mods.levelInVersus);
-                    }
-
-                    valY += 47;
+                    modifiers = GetAllModifiers();
+                    gotModifiers = true;
                 }
-                GUI.EndGroup();
+                CreateGUI();
             }
-            
-
-
         }
-        
-        public void MaxMod(Modifier mod)
-        {
-            
-            //WaveModifiers.Give(mod.data.name, mod.data.maxLevel);
-            Logger.LogMessage(mod.levelInSurvival);
-            Logger.LogMessage(mod.levelInVersus);
-        }
-        public List<Modifier> GetTotalNonMaxedModifiers()
-        {
-            var list = new List<Modifier>();
-            list.AddRange(ModifierManager.instance.GetVersusMods());
-            list.AddRange(ModifierManager.instance.GetNonMaxedSurvivalMods());
-            return list;
-        }
-        
+        Rect ModifierRect = new Rect(5, 5, 232f, 466);
 
-    }
-
-    [HarmonyPatch(typeof(ModifierManager), "GetNonMaxedSurvivalMods")]
-    internal class ModifierManagerGetNonMaxedSurvivalModsPatch
-    {
-        internal static List<Modifier> Mods { get; private set; } = new();
-    
-        internal static bool Prefix(ModifierManager __instance, ref List<Modifier> __result)
+        Vector2 scrollPosition;
+        // UI
+        private void CreateGUI()
         {
-            var templist = (from m in Traverse.Create(__instance).Field<List<Modifier>>("_modifiers").Value
-                where m.levelInSurvival < m.data.maxLevel && m.data.survival
-                select m).ToList();
-            if (Mods.Count > 0)
+            // Title Text && Background
+            GUI.Label(new Rect(63, 5, 55, 55), "ModifierModifier", new GUIStyle
             {
-                foreach (Modifier mod in Mods)
+                fontStyle = FontStyle.Bold,
+                normal =
                 {
-                    if (mod == null || templist.Contains(mod))
-                        continue;
-    
-                    templist.Add(mod);
+                    textColor = new Color(255,165,165, 255)
                 }
+            });
+            //GUI.Box(new Rect(5, 5, 232.5f, 24), "", );
+
+            GUILayout.BeginArea(ModifierRect, backgroundStyle);
+            {
+                overrideMaxLevel = GUI.Toggle(new Rect(5f, 30f, 16.4f, 16.4f), overrideMaxLevel, "", new GUIStyle
+                {
+                    alignment = TextAnchor.MiddleLeft,
+                    normal = new GUIStyleState
+                    {
+                        textColor = Color.gray,
+                        background = toggle
+                    },
+                    hover =
+                    {
+                        background = toggleHover
+                    },
+                    onHover =
+                    {
+                        background = toggleOnHover
+                    },
+                    active =
+                    {
+                        background = toggleActive
+                    },
+                    onActive =
+                    {
+                        background = toggleOnActive
+                    },
+                    onNormal =
+                    {
+                        background = toggleOn
+                    }
+                });
+
+                GUI.Label(new Rect(0f, 30f, 142.6f, 16.4f), "Bypass max level", new GUIStyle
+                {
+                    alignment = TextAnchor.MiddleCenter,
+                    fontSize = 16,
+                    fontStyle = FontStyle.Bold,
+                    contentOffset = new Vector2(22f, 0),
+                    normal =
+                    {
+                        textColor = Color.white
+                    }
+                });
+                GUI.Box(new Rect(5f, 55f, 222.5f, 2.5f), "", GUIStyle);
+                GUILayout.BeginArea(new Rect(5, 60, 232f, 402), GUIStyle.none);
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition,false,false, GUIStyle.none, GUIStyle.none, GUILayout.Width(362), GUILayout.Height(402)) ;
+                if (!gotModifiers) return;
+                foreach (Modifier mod in this.modifiers)
+                {
+                    GUILayout.FlexibleSpace();
+                    if(GUILayout.Button($"{mod.data.name} : {mod.levelInSurvival}", GUIStyle, GUILayout.Width(222.5f), GUILayout.Height(77f)))
+                    {
+                        HandleButtonClick(mod);
+                    }
+                }
+                GUILayout.EndArea();
+                GUILayout.EndScrollView();
             }
-    
-            __result = templist;
-            return false;
+            GUILayout.EndArea();
         }
-    
+
+        private void HandleButtonClick(Modifier modifier)
+        {
+            switch(Event.current.button)
+            {
+                case 0: // ADD
+                    if (overrideMaxLevel)
+                    {
+                        modifier.levelInSurvival++;
+                    }
+                    if (!overrideMaxLevel)
+                    {
+                        if (modifier.levelInSurvival > modifier.data.maxLevel) return;
+                        modifier.levelInSurvival++;
+                    }
+                    break;
+                case 1: // REMOVE
+                    if(modifier.levelInSurvival > 0)
+                    {
+                        modifier.levelInSurvival--;
+                    }
+                    break;
+            }
+            //if (overrideMaxLevel)
+            //{
+            //    modifier.levelInSurvival++;
+            //}
+            //if (!overrideMaxLevel)
+            //{
+            //    if (modifier.levelInSurvival > modifier.data.maxLevel) return;
+            //    modifier.levelInSurvival++;
+            //}
+        }
+
+        public List<Modifier> GetAllModifiers()
+        {
+            var a = Traverse.Create(ModifierManager.instance).Field<List<Modifier>>("_modifiers").Value;
+            return a;
+        }
+
         [HarmonyPatch(typeof(VersionNumberTextMesh), "Start")]
         public class VersionNumberTextMeshPatch
         {
             public static void Postfix(VersionNumberTextMesh __instance)
             {
                 __instance.textMesh.text +=
-                    $"\n<color=red>{Main.ModName} v{Main.ModVersion} by {Main.ModAuthor} \n use the INS or INSERT key to toggle the menu!!!</color>";
+                    $"\n<color=red>{Main.ModName} {Main.ModVersion} by {Main.ModAuthor} \n use the <color=white>INS</color> or <color=white>INSERT</color> key to toggle the menu!</color>";
             }
         }
     }
-    
-    // BIN
-    // ON GUI SHIT
-    // GUI.BeginGroup(new Rect(25f, 25f,Screen.width/2,Screen.height/2), Main.backgroundStyle);
-    // foreach (var mods in GetTotalNonMaxedModifiers())
-    // {
-    //
-    // }
-    // GUI.EndGroup();
+
 }
---
+++ ModifierModifier/StreamExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModifierModifier
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ReadAllBytes(this Stream instream)
+        {
+            if (instream is MemoryStream)
+                return ((MemoryStream)instream).ToArray();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                instream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
